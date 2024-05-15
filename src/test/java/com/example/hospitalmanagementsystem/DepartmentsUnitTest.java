1a2f32c53a44088954afb06dfd3377768e54a383
denisa-vasa <denisavasa86@gmail.com>
2024-05-15 09:22:17 +0200

    service commit

---
+++ src/test/java/com/example/hospitalmanagementsystem/DepartmentsUnitTest.java
@@ -0,0 +1,28 @@
+package com.example.hospitalmanagementsystem;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+public class DepartmentsUnitTest {
+
+    public DepartmentsUnitTest() {
+    }
+
+    @BeforeAll
+    public static void setUpClass() {
+    }
+
+    @AfterAll
+    public static void tearDownClass() {
+    }
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
