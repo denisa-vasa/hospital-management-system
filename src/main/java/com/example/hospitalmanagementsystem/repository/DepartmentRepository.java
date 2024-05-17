53aca417a4cea59090e2169e9a2380c4e3a8eef6
denisa-vasa <denisavasa86@gmail.com>
2024-05-17 21:56:45 +0200

    updated DepartmentManagementRepository

--- src/main/java/com/example/hospitalmanagementsystem/repository/DepartmentRepository.java
+++ src/main/java/com/example/hospitalmanagementsystem/repository/DepartmentRepository.java
@@ -2,10 +2,16 @@ package com.example.hospitalmanagementsystem.repository;
 
 import com.example.hospitalmanagementsystem.model.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
 
     Department findByName(String name);
+
+    @Query("SELECT d FROM Department d WHERE d.name LIKE %?1%")
+    public List<Department> filter(String filter);
 }
