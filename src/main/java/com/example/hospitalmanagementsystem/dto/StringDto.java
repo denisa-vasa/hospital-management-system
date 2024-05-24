58e85da63b65e6718d1c3ff039300f7660bee894
denisa-vasa <denisavasa86@gmail.com>
2024-05-24 08:42:12 +0200

    added StringDto

--- src/main/java/com/example/hospitalmanagementsystem/dto/StringDto.java
+++ src/main/java/com/example/hospitalmanagementsystem/dto/StringDto.java
@@ -2,8 +2,10 @@ package com.example.hospitalmanagementsystem.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class StringDto {
 
