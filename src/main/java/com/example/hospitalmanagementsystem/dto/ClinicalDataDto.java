9602427ab228c272744d35ea1c4c00956be3eaac
denisa-vasa <denisavasa86@gmail.com>
2024-06-04 16:02:41 +0200

    added ClinicalDataDto

--- src/main/java/com/example/hospitalmanagementsystem/dto/ClinicalDataDto.java
+++ src/main/java/com/example/hospitalmanagementsystem/dto/ClinicalDataDto.java
@@ -1,2 +1,16 @@
-package com.example.hospitalmanagementsystem.dto;public class ClinicalDataDto {
+package com.example.hospitalmanagementsystem.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ClinicalDataDto {
+    private Long id;
+    private String clinicalRecord;
+    private Long patientId;
+    private Long departmentId;
+    private Long admissionStateId;
 }
